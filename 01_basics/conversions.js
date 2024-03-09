537381fd012ce1151ee3a488f41368be052dfadf
manastamboli <126087139+manastamboli@users.noreply.github.com>
2024-03-09 09:00:26 +0000

    done

---
+++ 01_basics/conversions.js
@@ -0,0 +1,18 @@
+  let a= 1
+console.log(a)
+console.log(typeof(a))
+let value = Boolean(a)
+console.log(typeof(value))
+console.log(a)
+
+//OPERATIONSSS
+console.log(2**3)
+let str1 = "manas"
+let str2 = "Tamboli"
+console.log(str1+str2)
+console.log("2" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log("2" + 2) 
+
+// right clean code
+console.log(+true)
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"strict use";
+//number => 2 to the power fo 50
+//bollean = true or false
+//bigInt = bigger integers
+//Null => standalone value 
+// Undefined =>
+//symbol => unique
+
+//object
+console.log(typeof null)
+const num = 666
+let name = "manas"
+let surn = "tamboli"
+var type = true
+console.table([num,name,surn,type])
