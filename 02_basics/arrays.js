041ab334ef165dd2c815f2a9b663260937f7b180
manastamboli <126087139+manastamboli@users.noreply.github.com>
2024-03-11 13:01:04 +0000

    done

---
+++ 02_basics/arrays.js
@@ -0,0 +1,23 @@
+const myarray = [1,3,2,4,5]
+console.log(myarray)
+console.log("hey ")
+
+//array methods
+
+const newarr = [1,4,3,5,6,7]
+// newarr.push(11)
+// newarr.unshift(10)
+// console.log(newarr)
+// newarr.shift()
+// console.log(newarr)
+
+secarray = newarr.join()
+
+console.log(typeof secarray)
+console.log(secarray)
+
+console.log(secarray.slice(0,3))
+// console.log(secarray.Splice(0,3))
+// console.log("A",secarray)
+const my1 = secarray.splice(1,3)
+console.log(secarray)
